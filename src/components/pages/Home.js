2e4d5cd9e8362cefa91f0049e200620bcc6ba091
Kornél <leitner.kornel@gmail.com>
2020-05-09 18:45:55 +0200

    Reduce size of home page picture

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
       <div style={{ textAlign: 'center', marginTop: '50px' }}>
         <h1>Welcome to the PokéCyclopedia</h1>
         <img
-          style={{ marginTop: '50px' }}
+          style={{ marginTop: '50px', width: '350px', height: 'auto' }}
           src={'./favicon_pikachu256.png'}
           alt={'Pikatchu'}
           draggable='false'
