3c1ef57ed6dd1b25f15e89416ad9559474599a34
Kornél <leitner.kornel@gmail.com>
2020-05-10 16:31:15 +0200

    Rewrite App.js to functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/layout/Header';
@@ -10,99 +10,98 @@ import PokemonList from './components/pokemonlist/PokemonList';
 import TypeList from './components/typelist/TypeList';
 import PokemonDetail from './components/pokemondetail/PokemonDetail';
 
-class App extends Component {
-  state = {
-    pokemons: [],
-    types: [],
-    currentId: '',
-    name: '',
-    height: '',
-    weight: '',
-    base_experience: '',
-    pokemonTypes: [],
-    pokemonAbilities: [],
-  };
+const App = (props) => {
+  const [pokemons, setPokemons] = useState([]);
+  const [types, setTypes] = useState([]);
 
-  componentDidMount() {
+  const [pokemonTypes, setPokemonTypes] = useState([]);
+  const [pokemonAbilities, setPokemonAbilities] = useState([]);
+
+  const [currentId, setCurrentId] = useState('');
+  const [name, setName] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [baseExperience, setBaseExperience] = useState('');
+
+  useEffect(() => {
     axios
       .get('https://pokeapi.co/api/v2/pokemon')
-      .then((res) => this.setState({ pokemons: res.data.results }));
+      .then((res) => setPokemons(res.data.results));
     axios
       .get('https://pokeapi.co/api/v2/type')
-      .then((res) => this.setState({ types: res.data.results }));
-  }
+      .then((res) => setTypes(res.data.results));
+  }, []);
 
-  getDetails = (id) => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
-      this.setState({
-        currentId: res.data.id.toString(),
-        name: res.data.name,
-        height: res.data.height.toString(),
-        weight: res.data.weight.toString(),
-        base_experience: res.data.base_experience.toString(),
-        pokemonTypes: res.data.types.map((item) => {
+  const getDetails = (id) => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => {
+      setCurrentId(res.data.id.toString());
+      setName(res.data.name);
+      setHeight(res.data.height.toString());
+      setWeight(res.data.weight.toString());
+      setBaseExperience(res.data.base_experience.toString());
+      setPokemonTypes(
+        res.data.types.map((item) => {
           return item.type.name;
-        }),
-        pokemonAbilities: res.data.abilities.map((item) => {
+        })
+      );
+      setPokemonAbilities(
+        res.data.abilities.map((item) => {
           return item.ability.name;
-        }),
-      })
-    );
+        })
+      );
+    });
   };
 
-  render() {
-    return (
-      <Router>
-        <div className='App'>
-          <Header />
-          <Route
-            exact
-            path='/'
-            render={() => (
-              <React.Fragment>
-                <Home />
-              </React.Fragment>
-            )}
-          />
-          <Route
-            path='/pokemons'
-            render={(props) => (
-              <React.Fragment>
-                <PokemonList
-                  pokemons={this.state.pokemons}
-                  getDetails={this.getDetails}
-                />
-              </React.Fragment>
-            )}
-          />
-          <Route
-            path='/types'
-            render={(props) => (
-              <React.Fragment>
-                <TypeList types={this.state.types} />
-              </React.Fragment>
-            )}
-          />
-          <Route
-            path={'/pokemon/:pokemonId'}
-            render={(props) => (
-              <React.Fragment>
-                <PokemonDetail
-                  currentId={this.state.currentId}
-                  name={this.state.name}
-                  height={this.state.height}
-                  weight={this.state.weight}
-                  base_experience={this.state.base_experience}
-                  pokemonTypes={this.state.pokemonTypes}
-                  pokemonAbilities={this.state.pokemonAbilities}
-                />
-              </React.Fragment>
-            )}
-          />
-        </div>
-      </Router>
-    );
-  }
-}
+  let content = (
+    <Router>
+      <div className='App'>
+        <Header />
+        <Route
+          exact
+          path='/'
+          render={() => (
+            <React.Fragment>
+              <Home />
+            </React.Fragment>
+          )}
+        />
+        <Route
+          path='/pokemons'
+          render={(props) => (
+            <React.Fragment>
+              <PokemonList pokemons={pokemons} getDetails={getDetails} />
+            </React.Fragment>
+          )}
+        />
+        <Route
+          path='/types'
+          render={(props) => (
+            <React.Fragment>
+              <TypeList types={types} />
+            </React.Fragment>
+          )}
+        />
+        <Route
+          path={'/pokemon/:pokemonId'}
+          render={(props) => (
+            <React.Fragment>
+              <PokemonDetail
+                currentId={currentId}
+                name={name}
+                height={height}
+                weight={weight}
+                base_experience={baseExperience}
+                pokemonTypes={pokemonTypes}
+                pokemonAbilities={pokemonAbilities}
+              />
+            </React.Fragment>
+          )}
+        />
+      </div>
+    </Router>
+  );
+
+  return content;
+};
 
 export default App;
