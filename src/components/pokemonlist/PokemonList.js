89c869cf56adf094debb6daa804b2a8b459f7abc
Kornél <leitner.kornel@gmail.com>
2020-05-10 18:23:03 +0200

    Rewrite PokemonList.js to functional

--- src/components/pokemonlist/PokemonList.js
+++ src/components/pokemonlist/PokemonList.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PokemonCard from './PokemonCard';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 
-class PokemonList extends Component {
-  render() {
-    return (
-      <div style={containerStyle}>
-        <div style={pokemonListStyle} className='card-container'>
-          {this.props.pokemons.map((item) => (
-            <PokemonCard key={uuidv4()} pokemon={item} />
-          ))}
-        </div>
+const PokemonList = (props) => {
+  let content = (
+    <div style={containerStyle}>
+      <div style={pokemonListStyle} className='card-container'>
+        {props.pokemons.map((item) => (
+          <PokemonCard key={uuidv4()} pokemon={item} />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+
+  return content;
+};
 
 PokemonList.propTypes = {
   pokemons: PropTypes.array.isRequired,
