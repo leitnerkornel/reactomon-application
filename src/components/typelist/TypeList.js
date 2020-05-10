fd212eaaae32000c6bb843b6d68466884848d5f5
Kornél <leitner.kornel@gmail.com>
2020-05-10 18:31:19 +0200

    Rewrite TypeList.js to functional

--- src/components/typelist/TypeList.js
+++ src/components/typelist/TypeList.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TypeCard from './TypeCard';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 
-class TypeList extends Component {
-  render() {
-    return (
-      <div className='type-container'>
-        {this.props.types.map((item) => (
-          <TypeCard key={uuidv4()} type={item} />
-        ))}
-      </div>
-    );
-  }
-}
+const TypeList = (props) => {
+  let content = (
+    <div className='type-container'>
+      {props.types.map((item) => (
+        <TypeCard key={uuidv4()} type={item} />
+      ))}
+    </div>
+  );
+
+  return content;
+};
 
 TypeList.propTypes = {
   types: PropTypes.array.isRequired,
