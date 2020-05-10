5c4386c029e2e5702595f9a10313d8a43eb5e92d
Kornél <leitner.kornel@gmail.com>
2020-05-10 16:55:24 +0200

    Rewrite Home.js to functional

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Home extends Component {
-  render() {
-    return (
-      <div style={{ textAlign: 'center', marginTop: '50px' }}>
-        <h1>Welcome to the PokéCyclopedia</h1>
-        <img
-          style={{ marginTop: '50px', width: '350px', height: 'auto' }}
-          src={'./favicon_pikachu256.png'}
-          alt={'Pikatchu'}
-          draggable='false'
-        />
-      </div>
-    );
-  }
-}
+const Home = () => {
+  let content = (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to the PokéCyclopedia</h1>
+      <img
+        style={{ marginTop: '50px', width: '350px', height: 'auto' }}
+        src={'./favicon_pikachu256.png'}
+        alt={'Pikatchu'}
+        draggable='false'
+      />
+    </div>
+  );
+
+  return content;
+};
 
 export default Home;
