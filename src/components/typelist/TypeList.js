54a1150f6f0b2e3048d86caa6f2f00368db29341
Kornél <leitner.kornel@gmail.com>
2020-06-14 18:22:47 +0200

    Set display 4 rows and 5 columns, delete unecessary imports

--- src/components/typelist/TypeList.js
+++ src/components/typelist/TypeList.js
@@ -1,28 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import TypeCard from './TypeCard';
-import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import { API_POKEMON_TYPE_URL } from '../../Constants';
 
-const TypeList = (props) => {
+const TypeList = () => {
   const [types, setTypes] = useState([]);
 
   useEffect(() => {
     axios.get(API_POKEMON_TYPE_URL).then((res) => setTypes(res.data.results));
   }, []);
 
-  return (
-    <div className='type-container'>
-      {types.map((item) => (
-        <TypeCard key={uuidv4()} type={item} />
-      ))}
-    </div>
-  );
-};
+  const displayTypes = () => {
+    const rows = [];
+    const numberOfColumns = 5;
+    const numberOfRows = types.length / numberOfColumns;
 
-TypeList.propTypes = {
-  types: PropTypes.array.isRequired,
+    let counter = 0;
+
+    for (let rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
+      let row = [];
+      for (
+        let columnNumber = 0;
+        columnNumber < numberOfColumns;
+        columnNumber++
+      ) {
+        row.push(<TypeCard key={uuidv4()} type={types[counter++]} />);
+      }
+      rows.push(
+        <div key={uuidv4()} className='type-list-row'>
+          {row}
+        </div>
+      );
+    }
+
+    return rows;
+  };
+
+  return <div className='type-container'>{displayTypes()}</div>;
 };
 
 export default TypeList;
