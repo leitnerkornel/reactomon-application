166963cab7f455757abdddd90cc10dcbd11eb182
Kornél <leitner.kornel@gmail.com>
2020-06-14 17:51:23 +0200

    Move Type picture and title components and their style to TypeCard and App.css

--- src/components/typelist/TypeCard.js
+++ src/components/typelist/TypeCard.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import TypeCardTitle from './TypeCardTitle';
-import TypeCardPicture from './TypeCardPicture';
 import PropTypes from 'prop-types';
 
 const TypeCard = (props) => {
   const name = props.type.name;
   let content = (
-    <div style={typeCardStyle} className={'type-card'}>
-      <TypeCardPicture pictureName={name} />
-      <TypeCardTitle title={name} />
+    <div className={'type-card'}>
+      <div>
+        <img
+          className='type-image'
+          src={`/pokemon_types/${name}.png`}
+          alt={`This is: ${props.pictureName}`}
+          draggable='false'
+        />
+      </div>
+      <div>{name}</div>
     </div>
   );
 
@@ -17,12 +22,6 @@ const TypeCard = (props) => {
 
 TypeCard.propTypes = {
   type: PropTypes.object.isRequired,
-};
-
-const typeCardStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
 };
 
 export default TypeCard;
