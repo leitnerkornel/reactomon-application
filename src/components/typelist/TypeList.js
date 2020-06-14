590de29ce374e07f5dac058c70eebc493c77dca3
Kornél <leitner.kornel@gmail.com>
2020-06-14 17:42:41 +0200

    Put useEffect and useState into this component

--- src/components/typelist/TypeList.js
+++ src/components/typelist/TypeList.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import TypeCard from './TypeCard';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
+import { API_POKEMON_TYPE_URL } from '../../Constants';
 
 const TypeList = (props) => {
-  let content = (
+  const [types, setTypes] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_POKEMON_TYPE_URL).then((res) => setTypes(res.data.results));
+  }, []);
+
+  return (
     <div className='type-container'>
-      {props.types.map((item) => (
+      {types.map((item) => (
         <TypeCard key={uuidv4()} type={item} />
       ))}
     </div>
   );
-
-  return content;
 };
 
 TypeList.propTypes = {
