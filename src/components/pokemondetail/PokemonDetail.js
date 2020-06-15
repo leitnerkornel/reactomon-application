6572d0ecc780d0b0c94a31176702c2df7e4770d5
Kornél <leitner.kornel@gmail.com>
2020-06-15 14:35:30 +0200

    Change design on PokemonDetailPage

--- src/components/pokemondetail/PokemonDetail.js
+++ src/components/pokemondetail/PokemonDetail.js
@@ -1,55 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import PokemonDetailPicture from './PokemonDetailPicture';
 import PokemonDetailProperties from './PokemonDetailProperties';
 import PokemonDetailAbilities from './PokemonDetailAbilities';
 import PokemonDetailTypes from './PokemonDetailTypes';
+import { getPokemonIdFromWindowUrl, capitalize } from '../../Utils';
+import { API_PICTURE_URL, API_POKEMON_URL } from '../../Constants';
 
 const PokemonDetail = () => {
   const [pokemonTypes, setPokemonTypes] = useState([]);
   const [pokemonAbilities, setPokemonAbilities] = useState([]);
-
-  const [currentId, setCurrentId] = useState('');
   const [name, setName] = useState('');
   const [height, setHeight] = useState('');
   const [weight, setWeight] = useState('');
   const [baseExperience, setBaseExperience] = useState('');
 
-  const getPokemonIdFromUrl = () => {
-    let urlFragments = window.location.href.split('/');
-    return urlFragments[urlFragments.length - 1];
-  };
-
-  const currentPokemonId = getPokemonIdFromUrl();
+  const currentPokemonId = getPokemonIdFromWindowUrl();
 
   useEffect(() => {
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${currentPokemonId}`)
-      .then((res) => {
-        setCurrentId(res.data.id.toString());
-        setName(res.data.name);
-        setHeight(res.data.height.toString());
-        setWeight(res.data.weight.toString());
-        setBaseExperience(res.data.base_experience.toString());
-        setPokemonTypes(
-          res.data.types.map((item) => {
-            return item.type.name;
-          })
-        );
-        setPokemonAbilities(
-          res.data.abilities.map((item) => {
-            return item.ability.name;
-          })
-        );
-      });
+    axios.get(`${API_POKEMON_URL}/${currentPokemonId}`).then((res) => {
+      setName(res.data.name);
+      setHeight(res.data.height.toString());
+      setWeight(res.data.weight.toString());
+      setBaseExperience(res.data.base_experience.toString());
+      setPokemonTypes(
+        res.data.types.map((item) => {
+          return item.type.name;
+        })
+      );
+      setPokemonAbilities(
+        res.data.abilities.map((item) => {
+          return item.ability.name;
+        })
+      );
+    });
   }, [currentPokemonId]);
 
-  let content = (
-    <div style={{ textAlign: 'center' }}>
-      <PokemonDetailPicture pictureId={currentId} title={name} />
+  return (
+    <div className='detail-page-container' style={{ textAlign: 'center' }}>
+      <div className='detail-page-top-block'>
+        <div className='detail-page-image-container'>
+          <img
+            className='detail-page-image'
+            src={`${API_PICTURE_URL}${currentPokemonId}.png`}
+            alt={`This is: ${name}.`}
+            draggable='false'
+          />
+        </div>
+        <div className='name-catch-button-block'>
+          <div className='detail-pokemon-name'>
+            <h2>{capitalize(name)}</h2>
+          </div>
+          <div className='detail-page-catch-button'>
+            <div className='catch-button-item'>
+              <div className='detail-catch-button-image-container'>
+                <img
+                  className='pokeball-image'
+                  src='/pokeball.png'
+                  alt='Catch this pokemon!'
+                ></img>
+              </div>
+              <div className='detail-catch-text'>{'Catch!'.toUpperCase()}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <PokemonDetailProperties
-        name={name}
         experience={baseExperience}
         height={height}
         weight={weight}
@@ -58,8 +75,6 @@ const PokemonDetail = () => {
       <PokemonDetailTypes types={pokemonTypes} />
     </div>
   );
-
-  return content;
 };
 
 export default PokemonDetail;
