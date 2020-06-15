072dafc0d7dec35aa902f8b03521e806c1681b7b
Kornél <leitner.kornel@gmail.com>
2020-06-15 13:26:40 +0200

    Added getPokemonIdFromWindowUrl and capitalize util functions

--- src/Utils.js
+++ src/Utils.js
@@ -2,3 +2,12 @@ export const getPokemonIdFromUrl = (url) => {
   let urlArray = url.split('/');
   return parseInt(urlArray[urlArray.length - 2]);
 };
+
+export const getPokemonIdFromWindowUrl = () => {
+  let urlFragments = window.location.href.split('/');
+  return urlFragments[urlFragments.length - 1];
+};
+
+export const capitalize = (text) => {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+};
