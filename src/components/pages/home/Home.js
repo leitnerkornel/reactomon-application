81cc7a0e61c2be9a1466fed729529989ec365483
Kornel Leitner <leitner.kornel@gmail.com>
2020-09-20 15:20:14 +0200

    Change font style, delete empty lines

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,28 +1,27 @@
 import React from 'react';
 
-import "./Home.css";
 import HomeLinkButton from "./HomeLinkButton";
+import "./Home.css";
 
 const Home = () => {
   return (
-      <div className="home-container">
-        <img src={"./welcome_pokecyclopedia.png"} alt="Welcome to the PokéCyclopedia!" className="welcome-image" draggable='false'/>
-        <div className="home-link-container">
-
-            <p className="home-detail-text">On this site you can browse</p>
-            <HomeLinkButton route="pokemons" title="Pokémons"/>
-
-
-            <p className="home-detail-text">and their</p>
-            <HomeLinkButton route="types" title="Types"/>
-
-        </div>
-        <img className="home-image"
-             src={'./favicon_pikachu256.png'}
-             alt={'Pikatchu'}
-             draggable='false'
-        />
+    <div className="home-container">
+      <img className="welcome-image"
+           src={"./welcome_pokecyclopedia.png"}
+           alt="Welcome to the PokéCyclopedia!"
+           draggable='false'/>
+      <div className="home-link-container">
+        <p className="home-detail-text">On this site you can browse</p>
+        <HomeLinkButton route="pokemons" title="Pokémons"/>
+        <p className="home-detail-text">and their</p>
+        <HomeLinkButton route="types" title="Types"/>
       </div>
+      <img className="home-image"
+           src={'./favicon_pikachu256.png'}
+           alt={'Pikatchu'}
+           draggable='false'
+      />
+    </div>
   );
 };
 
