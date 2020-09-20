c99f2231e31275c167fa46c5c06cf117429477ce
Kornel Leitner <leitner.kornel@gmail.com>
2020-09-20 15:45:49 +0200

    Change image, add media queries, use CSS instead styles

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,54 +1,21 @@
 import React from 'react';
 import Navbar from './Navbar';
 
+import "./Header.css";
+
 const Header = () => {
   return (
-      <header style={headerStyle}>
-        <h1>
-          <span style={pokeStyle}>Poké</span>
-          <span style={cyclopediaStyle}>Cyclopedia</span>
-        </h1>
-        <Navbar/>
-      </header>
+    <header className="header-container">
+      <div className="header-image-container">
+        <img className="header-image"
+             src={"./pokecyclopedia_header.png"}
+             alt="PokéCyclopedia header"
+             draggable="false"
+        />
+      </div>
+      <Navbar/>
+    </header>
   );
-};
-
-const headerStyle = {
-  background: '#333',
-  color: '#fff',
-  textAlign: 'center',
-  width: '100%'
-};
-
-const cyclopediaStyle = {
-  MozUserSelect: 'none',
-  WebkitUserSelect: 'none',
-  msUserSelect: 'none',
-  userSelect: 'none',
-  OUserSelect: 'none',
-  unselectable: 'on',
-  onselectstart: 'return false',
-  onmousedown: 'return false',
-  padding: '5px',
-  borderRadius: '15px',
-  background: '#f00',
-  color: '#fff',
-};
-
-const pokeStyle = {
-  MozUserSelect: 'none',
-  WebkitUserSelect: 'none',
-  msUserSelect: 'none',
-  userSelect: 'none',
-  OUserSelect: 'none',
-  unselectable: 'on',
-  onselectstart: 'return false',
-  onmousedown: 'return false',
-  fontStyle: 'italic',
-  padding: '5px',
-  borderRadius: '15px',
-  background: '#fff',
-  color: '#f00',
 };
 
 export default Header;
