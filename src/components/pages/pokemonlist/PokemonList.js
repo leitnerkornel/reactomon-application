8c1bb260692af165a7f37d8c3447abcd0733b99c
Kornel Leitner <leitner.kornel@gmail.com>
2024-09-22 19:51:36 +0200

    31 - Fix responsive issues * Fix grid at PokemonList

--- src/components/pages/pokemonlist/PokemonList.js
+++ src/components/pages/pokemonlist/PokemonList.js
@@ -18,31 +18,6 @@ const PokemonList = () => {
       .then((res) => setPokemons(res.data.results));
   }, [limit, offset]);
 
-  const displayRows = () => {
-    const rows = [];
-    const numberOfColumns = 3;
-    const numberOfRows = pokemons.length / numberOfColumns;
-    let counter = 0;
-
-    for (let rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
-      let row = [];
-      for (
-        let columnNumber = 0;
-        columnNumber < numberOfColumns;
-        columnNumber++
-      ) {
-        row.push(<PokemonCard key={uuidv4()} pokemon={pokemons[counter++]} />);
-      }
-      rows.push(
-        <div key={uuidv4()} className='pokemon-list-row'>
-          {row}
-        </div>
-      );
-    }
-
-    return rows;
-  };
-
   return <div className='pokemon-card-container'>{pokemons.map((pokemon) => {
     return <PokemonCard key={uuidv4()} pokemon={pokemon}/>
   })}</div>;
