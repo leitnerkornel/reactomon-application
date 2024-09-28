6729012ece08171148a72bc8d34d4915e58f4c18
Kornel Leitner <leitner.kornel@gmail.com>
2024-09-28 15:46:02 +0200

    31 - Fix responsive issues * Fix missing types * Added new images

--- src/components/typelist/TypeCard.js
+++ src/components/typelist/TypeCard.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 
 const TypeCard = (props) => {
   const name = props.type.name;
-  let content = (
+
+  return (
     <div className={'type-card'}>
       <div>
         <img
@@ -11,13 +12,15 @@ const TypeCard = (props) => {
           src={`/pokemon_types/${name}.png`}
           alt={`This is: ${name}`}
           draggable='false'
+          onError={({currentTarget}) => {
+            currentTarget.onerror = null;
+            currentTarget.src = '/pokemon_types/pokemon_type_not_found.png';
+          }}
         />
       </div>
       <div className='type-text'>{name}</div>
     </div>
   );
-
-  return content;
 };
 
 TypeCard.propTypes = {
