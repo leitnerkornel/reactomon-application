fb779c82ce05621094ab92a84843452fab6e3bf1
Kornel Leitner <leitner.kornel@gmail.com>
2024-10-16 21:46:56 +0200

    44 - Image lazy load and default images * New component for Pokemon images at List page * Handle error and animated loading

---
+++ src/components/pages/pokemonlist/PokemonCardImage.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+import './PokemonCardImage.css'
+
+const PokemonCardImage = ({ imageSrc, pokemonName }) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  return (
+    <img
+      className={`${isLoaded ? 'loaded' : ''} pokemon-card-image`}
+      src={imageSrc}
+      alt={`This is: ${pokemonName}.`}
+      draggable="false"
+      onError={({ currentTarget }) => {
+        currentTarget.onerror = null
+        currentTarget.src = '/pokemon_not_found.png'
+      }}
+      onLoad={() => {
+        setIsLoaded(true)
+      }}
+    />
+  )
+}
+export default PokemonCardImage
